f9135f5aae8b0636ee27e13ca811cb73684c6804
c6197 <11230227@sssh.tp.edu.tw>
2024-04-11 11:28:28 +0800

    entrace

---
+++ Entrance/data.js
@@ -0,0 +1,239 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "門口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2816,
+      "initialViewParameters": {
+        "yaw": -1.0910191348136173,
+        "pitch": 0.045676937861138356,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3043015394878275,
+          "pitch": 0.16514207804405778,
+          "rotation": 4.71238898038469,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-",
+      "name": "入口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "yaw": 0.18685916034410255,
+        "pitch": 0.3094925120076706,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0674442848179755,
+          "pitch": 0.31040937984273853,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": -0.07862001765614934,
+          "pitch": 0.38108702167925657,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "售票處",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.116804739583566,
+          "pitch": 0.5644810138103971,
+          "rotation": 4.71238898038469,
+          "target": "1-"
+        },
+        {
+          "yaw": 0.08504781691418373,
+          "pitch": 0.19124638123017235,
+          "rotation": 1.5707963267948966,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.9748520351398255,
+          "pitch": 0.1463245409785916,
+          "title": "售票亭",
+          "text": "一人30塊"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "金庫",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.47775171752791934,
+        "pitch": 0.50040612142379,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.03245830379725234,
+          "pitch": 0.8392979163119776,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.1432303660425038,
+          "pitch": 0.2806814891556719,
+          "title": "金庫",
+          "text": "日治時期金庫改建"
+        }
+      ]
+    },
+    {
+      "id": "4-ii",
+      "name": "金庫II",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.114979002996904,
+        "pitch": 0.15789960198896225,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.06045594475785876,
+          "pitch": 0.6294527176052718,
+          "rotation": 4.71238898038469,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Entrance",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
