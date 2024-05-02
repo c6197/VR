fceac09da95ba87894dab5e6cd302491969a25f7
c6197 <11230227@sssh.tp.edu.tw>
2024-05-02 11:59:09 +0800

    5

---
+++ Entrace/data.js
@@ -0,0 +1,232 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "大門",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2816,
+      "initialViewParameters": {
+        "yaw": 1.0333855756828534,
+        "pitch": -0.09962255862496505,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.329488642873697,
+          "pitch": 0.07040917236199462,
+          "rotation": 4.71238898038469,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-",
+      "name": "走廊",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1076990042170003,
+          "pitch": 0.21288428394903391,
+          "rotation": 1.5707963267948966,
+          "target": "0-"
+        },
+        {
+          "yaw": 0.0020725487331301196,
+          "pitch": 0.1882976918044328,
+          "rotation": 1.5707963267948966,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "售票台",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.057793803945756395,
+          "pitch": 0.15543106754958558,
+          "rotation": 1.5707963267948966,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.9246264632921992,
+          "pitch": 0.22598901965589846,
+          "title": "售票台",
+          "text": "一人30"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "金庫",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.02493327502849141,
+        "pitch": 0.1704654875263394,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8424953274599005,
+          "pitch": 0.5285577685796135,
+          "rotation": 4.71238898038469,
+          "target": "2-"
+        },
+        {
+          "yaw": -0.05006114818439755,
+          "pitch": 0.5324488134884504,
+          "rotation": 4.71238898038469,
+          "target": "4-ii"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-ii",
+      "name": "金庫II",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.02767802386312823,
+          "pitch": 0.7320365796516821,
+          "rotation": 1.5707963267948966,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
---
+++ Exhibition hall/data.js
@@ -0,0 +1,216 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "展廳",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.10329499654660523,
+        "pitch": 0.11194748434565582,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.29293116634888605,
+          "pitch": 0.17104132756485413,
+          "rotation": 0,
+          "target": "1-ii"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-ii",
+      "name": "展廳II",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "yaw": -2.3620230479912063,
+        "pitch": -0.24401776029889888,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5786826724792888,
+          "pitch": 0.20032662559113845,
+          "rotation": 4.71238898038469,
+          "target": "2-iii"
+        },
+        {
+          "yaw": -3.102102706037593,
+          "pitch": 0.19984420491963562,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.0214539379698078,
+          "pitch": -0.371289282740074,
+          "title": "霸王龍",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "2-iii",
+      "name": "展廳III",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.977740570760087,
+        "pitch": -0.04834095914925385,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8189520696637071,
+          "pitch": 0.2096259478350948,
+          "rotation": 6.283185307179586,
+          "target": "1-ii"
+        },
+        {
+          "yaw": -1.6253944056923082,
+          "pitch": 0.21575032620838286,
+          "rotation": 0,
+          "target": "3-iv"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.4051351012866604,
+          "pitch": -0.12459081205985356,
+          "title": "梁龍",
+          "text": ""
+        },
+        {
+          "yaw": 1.434232190689733,
+          "pitch": -0.34143463228675053,
+          "title": "翼龍",
+          "text": ""
+        },
+        {
+          "yaw": -1.1707142607231695,
+          "pitch": -0.11514077759069608,
+          "title": "滄龍",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "3-iv",
+      "name": "展廳IV",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "yaw": 2.6594362801938995,
+        "pitch": 0.15149917819505454,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.127088442963501,
+          "pitch": 0.3233584751776064,
+          "rotation": 4.71238898038469,
+          "target": "2-iii"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.525147709974952,
+          "pitch": 0.09121870072186944,
+          "title": "長毛象",
+          "text": ""
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
